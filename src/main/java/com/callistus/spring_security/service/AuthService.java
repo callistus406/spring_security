8f5f3ee3aef02b01f0125bec587046e0042a8ca9
callistus <callistus406@gmail.com>
2024-12-26 21:37:19 +0100

    Update

--- src/main/java/com/callistus/spring_security/service/AuthService.java
+++ src/main/java/com/callistus/spring_security/service/AuthService.java
@@ -1,9 +1,15 @@
 package com.callistus.spring_security.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.callistus.spring_security.dto.LoginDto;
 import com.callistus.spring_security.dto.RegisterDto;
+import com.callistus.spring_security.dto.UserDto;
 import com.callistus.spring_security.model.User;
 import com.callistus.spring_security.repository.UserRepository;
 
@@ -12,6 +18,10 @@ public class AuthService {
 
     private UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     // private RegisterDto registerDto;
 
     public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
@@ -24,6 +34,24 @@ public class AuthService {
         User user = new User().setName(registerDto.getName()).setEmail(registerDto.getEmail())
                 .setPassword(this.passwordEncoder.encode(registerDto.getPassword())).setPhone(registerDto.getPhone());
         return userRepository.save(user);
+
+    }
+
+    public User signIn(LoginDto loginDto) {
+        authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword()));
+
+        return userRepository.findByEmail(loginDto.getEmail()).orElseThrow();
+
+    }
+
+    public User me(String email) {
+
+        var user = this.userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+                System.out.println("==================");
+        return user;
 
     }
 
