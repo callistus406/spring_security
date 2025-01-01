7e268ef9a41c8e062af729859940f7df686f7119
callistus <callistus406@gmail.com>
2025-01-01 02:40:55 +0100

    update

--- src/main/java/com/callistus/spring_security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/callistus/spring_security/config/JwtAuthenticationFilter.java
@@ -23,7 +23,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    // private final HandlerExceptionResolver exceptionResolver;
     private JwtService jwtService;
     private CustomUserDetailsService customUserDetailsService;
 
