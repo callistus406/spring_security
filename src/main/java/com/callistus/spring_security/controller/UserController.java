7599336bd636193c2c9c1d3c9d2a4e3c143dc0b5
callistus <callistus406@gmail.com>
2025-01-04 17:27:56 +0100

    ctrl update

--- src/main/java/com/callistus/spring_security/controller/UserController.java
+++ src/main/java/com/callistus/spring_security/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.callistus.spring_security.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.callistus.spring_security.dto.UserDto;
 import com.callistus.spring_security.model.User;
 import com.callistus.spring_security.service.AuthService;
+import com.callistus.spring_security.service.CustomUserDetailsService;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class UserController {
@@ -17,12 +23,21 @@ public class UserController {
     @Autowired
     AuthService authService;
 
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
+
       @GetMapping("/me")
-    public UserDto  login(@RequestBody String email) {
-        
+      public UserDto login(@RequestBody String email) {
+
         User user = authService.me(email);
         System.out.println(user.getEmail());
         UserDto userDto = new UserDto(user.getEmail(), user.getName());
         return userDto;
-    }
+      }
+    
+      @GetMapping("/users")
+      public List<User> getMethodName() {
+        return this.customUserDetailsService.findAllUsers();
+      }
+      
 }
