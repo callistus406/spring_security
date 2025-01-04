f971f16cb22ab36bd8f5a765c1f8137c17d9eaa0
callistus <callistus406@gmail.com>
2025-01-04 17:25:15 +0100

    service update

--- src/main/java/com/callistus/spring_security/service/CustomUserDetailsService.java
+++ src/main/java/com/callistus/spring_security/service/CustomUserDetailsService.java
@@ -1,10 +1,13 @@
 package com.callistus.spring_security.service;
 
+import java.util.List;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.callistus.spring_security.model.User;
 import com.callistus.spring_security.repository.UserRepository;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -22,6 +25,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
 
-    
+    public List<User> findAllUsers() {
+        
+        return userRepository.find();
+    }
 
 }
